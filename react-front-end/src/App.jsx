2c5ef86b0fe6529bb36a623446b7633792390361
victorcodigos <victor.macedo.dedeus@gmail.com>
2023-08-03 22:07:11 +0200

    Component Profile is done

--- react-front-end/src/App.jsx
+++ react-front-end/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from './components/Login/Login'
 import { UserProvider } from './context/UserContext/UserState'
+import Profile from './components/Profile/Profile'
 import './App.css'
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <UserProvider>
           <Routes>
             <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
           </Routes>
         </UserProvider>
       </BrowserRouter>
