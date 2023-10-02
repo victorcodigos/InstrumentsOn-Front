9a52b686e4e06a5d02c3e3ae34b42878a215ee74
victorcodigos <victor.macedo.dedeus@gmail.com>
2023-10-03 00:08:21 +0200

    Lets continue working in the web

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
 import './index.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 import "@madzadev/audio-player/dist/index.js"
 
 
